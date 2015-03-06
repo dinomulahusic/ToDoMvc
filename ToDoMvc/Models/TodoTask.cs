6cd247325b9240752cac0c005111a2ac67c2a99b
dinomulahusic <dinomulahusic@gmail.com>
2015-03-06 23:24:39 +0100

    Added server side tasks and jquery invokations

---
+++ ToDoMvc/Models/TodoTask.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ToDoMvc.Models
+{
+    public class TodoTask
+    {
+        public int TodoTaskID { get; set; }
+        public string Title { get; set; }
+        public string Date { get; set; }
+    }
+}
--- ToDoMvc/Scripts/app.js
+++ ToDoMvc/Scripts/app.js
@@ -1,7 +1,22 @@
 ﻿var main = function () {
+    $('td').each(function (i, el) {
+        loadData(el.id);
+    });
+
     $('#todo-table td').sortable({
         connectWith: ".connectedSortable"
     }).disableSelection();
+};
+
+var loadData = function (dateParam) {
+    $.getJSON("/Home/GetTodoTasks", { date: dateParam }, function (data) {
+        var dayColumn = $('#' + dateParam);
+        dayColumn.children().remove();
+
+        $.each(data, function (i, item) {
+            dayColumn.append('<div class="sticky">' + item.Title + '</div>')
+        });
+    });
 };
 
 $(document).ready(main);
